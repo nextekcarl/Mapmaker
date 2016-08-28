608033f8465355af2d7fb2fc1a7441b5b7c889f7
Carl Anderson <nexusrpg@gmail.com>
2016-08-27 18:42:43 -0700

    Added loading icon to ajax actions for UX. Tried to add it for generate Maps function. Failed.

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -1,19 +1,31 @@
+$(function ($){
+    $(document).ajaxStop(function(){
+        $("#loading-indicator").hide();
+     });
+     $(document).ajaxStart(function(){
+         $("#loading-indicator").show();
+     });
+});
+
+function generateMap() {
+  var detailedParams = {
+      extent: defaultExtent,
+      generator: generateCoast,
+      npts: Math.floor((Math.random() * 10) + 20) * 1000,
+      ncities: Math.floor((Math.random() * 8) + 5),
+      nterrs: Math.floor((Math.random() * 10) + 1),
+      mountains: Math.floor((Math.random() * 70) + 30),
+      fontsizes: {
+          region: 30,
+          city: 20,
+          town: 15
+      }
+  }
+  doMap(d3.select('#map'), detailedParams);
+}
 $(document).on('turbolinks:load', function() {
   $('body').on("click", '#generate-map', function () {
-    var detailedParams = {
-        extent: defaultExtent,
-        generator: generateCoast,
-        npts: Math.floor((Math.random() * 10) + 20) * 1000,
-        ncities: Math.floor((Math.random() * 8) + 5),
-        nterrs: Math.floor((Math.random() * 10) + 1),
-        mountains: Math.floor((Math.random() * 70) + 30),
-        fontsizes: {
-            region: 30,
-            city: 20,
-            town: 15
-        }
-    }
-    doMap(d3.select('#map'), detailedParams);
+    setTimeout(generateMap(), 0);
     $('body text.region').show();
     $('body .city').show();
     $('body path.border').show();
@@ -34,6 +46,7 @@ $(document).on('turbolinks:load', function() {
   });
 
   $('body').on("click", '#save-map', function () {
+    $('#loading-indicator').show();
     //Package up the name, description, and svg data,
     //ajax off to rails action, and respond to user.
     var $form = $('#map-form').children('form');
@@ -44,9 +57,7 @@ $(document).on('turbolinks:load', function() {
       url: "/maps",
       data: $form.serialize() + '&map%5Bsvg%5D=' + $svgSerialized
     }).success({
-
     }).fail({
-
     });
   });
 
