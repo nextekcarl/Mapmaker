6e551d4a0b57ef61b63cc732ec9a0506d47d2f75
Carl Anderson <nexusrpg@gmail.com>
2016-08-28 20:05:55 -0700

    Added About page

---
+++ app/controllers/about_controller.rb
@@ -0,0 +1,6 @@
+class AboutController < ApplicationController
+
+  def index
+    #code
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :maps, only: [:index, :new, :create, :show]
+  resources :about, only: [:index]
   root 'maps#index'
 end
